4a945efe044b722266d7beb6d4216a13c93b3ece
Eric Zhang <zhangeric.zhang@gmail.com>
2025-02-08 15:56:17 -0800

    Added components.
    Created components for Definitions tab, ResearchList (for displaying history of research papers), and Vote Buttons.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./components/Home/Home"
 import LogIn from './components/LogIn/LogIn';
+import Definitions from './components/Definitions/Definitions';
+import ResearchList from './components/ResearchList/ResearchList';
+import VoteButtons from './components/VoteButtons/VoteButtons';
+
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<LogIn/>} />
+          <Route path="/research" element={<ResearchList/>} />
+          <Route path="/buttons" element={<VoteButtons/>} />
+          <Route path="/definitions" element={<Definitions/>} />
         </Routes>
     </Router>
   );
